5fab1c70e9c18d3f0ef63f87753ae94e9d212ac2
Juan Fernando Rios <juan.rios27@udea.edu.co>
2021-10-02 12:28:52 -0500

    Test de servicios

--- middlewares/validate-number-digits.js
+++ middlewares/validate-number-digits.js
@@ -4,7 +4,6 @@ const validateNumberDigits = (req, res = response, next ) => {
 
     const { number = '1234' } = req.query;
 
-    console.log(number.length)
     if(number.length !== 4) {
         return res.status(400).json({
             msg: 'El número debe ser de 4 dígitos'
