b8c85e009c8537a4a1d289e56d79d8123559eff6
Juan Fernando Rios <juan.rios27@udea.edu.co>
2021-10-02 12:11:20 -0500

    Configuración para test de servicios

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+const express = require('express');
 require('dotenv').config();
-const Server = require('./models/server');
+const cors = require('cors');
+
+// Crear el servidor de express
+const app = express();
+
+// CORS
+app.use(cors())
+
+// Directorio Público
+app.use( express.static('public') );
+
+// Lectura y parseo del body
+app.use( express.json() );
+
+// Rutas
+app.use('/api/v1/codebreaker', require('./routes/code-breaker') );
 
 
-const server = new Server();
+// Escuchar peticiones
+// app.listen( process.env.PORT, () => {
+//     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+// });
 
-
-
-server.listen();
+module.exports = app;
